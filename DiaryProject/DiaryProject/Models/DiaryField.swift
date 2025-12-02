2a08a27e2a2fd51a9eaed45c6267032d2de846c0
Juliana Pavan <julianapavan@MacBook-Pro-de-Juliana.local>
2025-12-02 10:56:27 -0300

    Add viewModel

--- DiaryProject/DiaryProject/Models/DiaryField.swift
+++ DiaryProject/DiaryProject/Models/DiaryField.swift
@@ -1,9 +1,35 @@
+import Foundation
 import SwiftUI
 
 struct DiaryField: Identifiable {
-    let id: Int
+    var id: UUID = UUID()
     let icon: Image
     let title: String
     let date: String
     let text: String
+}
+
+@Observable
+class DiaryViewModel {
+    static let shared = DiaryViewModel()
+    
+    var fields: [DiaryField] = [
+        DiaryField(icon: Image(systemName: "heart.fill"),
+                   title: "Magic Kingdom",
+                   date: "24/11/25",
+                   text:  "This is halloween, this is halloween"),
+        DiaryField(icon: Image(systemName: "star.fill"),
+                   title: "Epcot",
+                   date: "24/11/25",
+                   text:  "O que é aquela atração dos guardiões??? Melhor brinquedoo!!!")
+    ]
+    
+    func addField(icon: Image, title: String, text: String) {
+        let date = Date.now.formatted(date: .abbreviated, time: .omitted)
+        let newField = DiaryField(icon: icon,
+                                  title: title,
+                                  date: date,
+                                  text: text)
+        DiaryViewModel.shared.fields.append(newField)
+    }
 }
