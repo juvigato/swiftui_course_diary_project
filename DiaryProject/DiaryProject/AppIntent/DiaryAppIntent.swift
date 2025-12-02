23c23317bda06572dccb747e1a0c533be6a13f46
Juliana Pavan <julianapavan@MacBook-Pro-de-Juliana.local>
2025-12-02 11:08:24 -0300

    Add app intent

---
+++ DiaryProject/DiaryProject/AppIntent/DiaryAppIntent.swift
@@ -0,0 +1,38 @@
+//
+//  DiaryAppIntent.swift
+//  DiaryProject
+//
+//  Created by Juliana Pavan on 02/12/25.
+//
+
+import AppIntents
+import SwiftUI
+
+struct DiaryAppIntent: AppIntent {
+    static var title: LocalizedStringResource = "Bora escrever!"
+    static var description = IntentDescription(stringLiteral: "Escreva no seu diário ;)")
+    
+    @Parameter(title: "Título")
+    var title: String
+    
+    @Parameter(title: "Nome do ícone")
+    var iconName: String
+    
+    @Parameter(title: "Texto")
+    var text: String
+    
+    init() { }
+    
+    init(title: String, iconName: String, text: String) {
+        self.title = title
+        self.iconName = iconName
+        self.text = text
+    }
+    
+    func perform() async throws -> some IntentResult {
+        DiaryViewModel.shared.addField(icon: Image(systemName: iconName),
+                                       title: title,
+                                       text: title)
+        return .result()
+    }
+}
